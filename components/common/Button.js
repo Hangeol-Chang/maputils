7708ed66c54c478d38712d8bc542a08bda4ac60a
Hangeol-Chang <hgchang1@naver.com>
2023-02-13 07:58:12 +0900

    config

---
+++ components/common/Button.js
@@ -0,0 +1,32 @@
+
+export default function Button(props) {
+    const buttonConfig = {
+        // params : color
+        primary : {
+            bgColor : 'bg-blue-300',
+            color : 'text-white',
+            outline : ''
+        },
+        primary_outline : {
+            bgColor : 'bg-white',
+            color : 'text-blue-300',
+            outline : ''
+        }
+
+        // params : size
+    }
+
+    return(
+        <button
+            className={`
+                ${props.borderRadius}
+
+                ${buttonConfig[props.color].bgColor}
+                ${buttonConfig[props.color].color}
+                ${buttonConfig[props.color].outline}
+            `}
+        >
+            {props.value}
+        </button>
+    )
+}
