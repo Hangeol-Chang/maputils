f878d01eeab08bcf37cb93d4f8457d6d55d37876
Hangeol-Chang <hgchang1@naver.com>
2023-02-22 08:46:53 +0900

    path에 arrow 추가

--- components/pathdrawer/LineComponent.js
+++ components/pathdrawer/LineComponent.js
@@ -5,12 +5,22 @@ export default function LineComponent({ option }) {
 
     return (
         <>
+            {/* main path */}
             <PolylineF path={option.path} options={option.lineOption} />
+
+            {/* marker */}
             {
                 option.path.map((c, idx) => (
                     <CircleF key={idx} center={c} options={option.circleOption} />
                 ))
-            }   
+            }
+
+            {/* arrow */}
+            {
+                option.arrows.map((p, idx) => (
+                    <PolylineF path={p} key={idx} options={option.arrowOption} />
+                ))
+            }
         </>
     )
 }
