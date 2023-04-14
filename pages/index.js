e82c847dbe0457f7112bc6841b641655cadced86
ChangHangeol <hgchang1@naver.com>
2023-04-15 01:22:27 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import {useRouter} from 'next/router'
 import Button from '../components/common/Button'
 import styles from '../styles/Home.module.css'
 import { useRecoilValue } from 'recoil';
+import { relativePrefixState } from '../components/states/state';
 
 export default function Home() {
     const router = useRouter();
