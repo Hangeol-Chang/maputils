401d1512ca150155e46a64afea82d3de7df75e79
ChangHangeol <hgchang1@naver.com>
2023-04-25 12:43:47 +0900

    version1 release point

--- components/pathdrawer/LineComponentNaver.js
+++ components/pathdrawer/LineComponentNaver.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
-import { Polyline } from "react-naver-maps"
+import { Circle, Marker, Polyline } from "react-naver-maps"
 
 export default function LineComponentNaver({ idf, option, navermaps }) {
     const [path, setPath] = useState([])
+    const [arrowPath, setArrowPath] = useState([])
 
     useEffect(() => {
         let tmppath = []
@@ -10,6 +11,19 @@ export default function LineComponentNaver({ idf, option, navermaps }) {
             tmppath.push(new navermaps.LatLng(g.lat, g.lng))
         })
         setPath(tmppath)
+
+        const arrows = option.arrows;
+
+        let tmparrows = [];
+        arrows.map((arrow) => {
+            let one = [] 
+            arrow.map((g) => {
+                one.push(new navermaps.LatLng(g.lat, g.lng))
+            })
+            tmparrows.push(one)
+        })
+        setArrowPath(tmparrows);
+        console.log(tmparrows);
     }, [])
 
     // useEffect(() => {
@@ -24,7 +38,34 @@ export default function LineComponentNaver({ idf, option, navermaps }) {
         <>
             <Polyline 
                 path={path}
+                strokeColor={option.lineOption.strokeColor}
+                strokeWeight={option.lineOption.strokeWeight}
+                strokeOpacity={option.lineOption.strokeOpacity}
+                visible={option.lineOption.visible}
             />
+
+            {
+                path.map((coordi) => {
+                    <Circle
+                        center={coordi}
+                        radius={option.circleOption.radius}
+                        fillColor={option.circleOption.fillColor}
+                        fillOpacity={option.circleOption.fillOpacity}
+                        visible={option.circleOption.visible}
+                    />
+                })
+            }
+            {
+                arrowPath.map((arrows) => {
+                    <Polyline
+                        path={arrows}
+                        strokeColor={option.arrowOption.strokeColor}
+                        strokeWeight={option.arrowOption.strokeWeight}
+                        strokeOpacity={option.arrowOption.strokeOpacity}
+                        visible={option.arrowOption.visible}
+                    />
+                })
+            }
         </>
     )
 }
